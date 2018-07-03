519b772ab7c80156d9a303b398ea297120379855
Shogo Sawai <shogo.sawai@gmail.com>
2018-07-03 19:23:01 +0900

    fix openssl version diff
    The default digest was changed from MD5 to SHA256 in Openssl 1.1

--- .gitencrypt/smudge_filter_openssl.sh
+++ .gitencrypt/smudge_filter_openssl.sh
@@ -5,4 +5,4 @@ PASS_FIXED="pass:`tail -n 1 .password`"
 
 # If decryption fails, use `cat` instead. 
 # Error messages are redirected to /dev/null.
-openssl enc -d -base64 -aes-256-ecb -k $PASS_FIXED 2> /dev/null || cat
+openssl enc -md md5 -d -base64 -aes-256-ecb -k $PASS_FIXED 2> /dev/null || cat
