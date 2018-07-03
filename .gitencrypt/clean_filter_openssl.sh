16e87c2b617d8966b1683d943cb0a8a5073776f6
Shogo Sawai <shogo.sawai@gmail.com>
2018-07-03 19:22:14 +0900

    fix openssl version diff
    The default digest was changed from MD5 to SHA256 in Openssl 1.1

--- .gitencrypt/clean_filter_openssl.sh
+++ .gitencrypt/clean_filter_openssl.sh
@@ -3,4 +3,4 @@
 SALT_FIXED=`head -n 1 .password` # 24 or less hex characters
 PASS_FIXED="pass:`tail -n 1 .password`"
 
-openssl enc -base64 -aes-256-ecb -S $SALT_FIXED -k $PASS_FIXED
+openssl enc -md md5 -base64 -aes-256-ecb -S $SALT_FIXED -k $PASS_FIXED
